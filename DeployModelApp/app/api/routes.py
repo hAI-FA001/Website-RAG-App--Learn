6971e22bb8195e448f7d1dee782179e8d200b08d
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-07-27 07:08:45 +0500

    chunks related route done

--- DeployModelApp/app/api/routes.py
+++ DeployModelApp/app/api/routes.py
@@ -1,9 +1,27 @@
 from . import api_blueprint
+from flask import request, jsonify
+from app.services import scraping_service, model_service, pinecone_service
+from app.utils.helper_functions import make_chunks
+
+import os
+from dotenv import load_dotenv
+
+PINECONE_INDEX_NAME = os.environ['PINECONE_INDEX_NAME']
 
 @api_blueprint.route('/embed-and-store', methods=['POST'])
 def embed_and_store():
     # scrape the url, generate embedding, upload to vector db
-    pass
+    url = request.json["url"]
+    url_txt = scraping_service.scrape(url)
+    
+    chs = make_chunks(url_txt)
+    pinecone_service.embed_and_upload(chs, PINECONE_INDEX_NAME)
+
+    res_json = {
+        "message": "Chunks embedded and uploaded to Pinecone"
+    }
+
+    return jsonify(res_json)
 
 @api_blueprint.route('/handle-query', methods=['POST'])
 def handle_query():
